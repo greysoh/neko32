76c09c7d65cb1078d59a02eb92cdc7f26f1b1ade
greysoh <me@greysoh.dev>
2024-03-06 14:59:18 -0500

    chore: Starts prototyping CPU.

--- cpu/src/main.rs
+++ cpu/src/main.rs
@@ -1,5 +1,45 @@
 use std::env;
+use std::fs;
+use std::process::exit;
+
+mod cpu;
+
+const MEMORY_SIZE: usize = 128 * 1024;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        println!("usage: <emu> <bin file>.bin");
+        exit(1);
+    }
+
+    let file_contents = fs::read(&args[1]).expect("Should have been able to read the file");
+
+    if file_contents.len() > 4096 {
+        println!("file is too big to be usable. must be < 4096 bytes");
+        exit(1);
+    }
+
+    println!("intializing cpu...");
+    let memory: &mut [u8; MEMORY_SIZE] = &mut [0; MEMORY_SIZE];
+    memory.copy_from_slice(&file_contents[..]);
+
+    let registers: &mut [u32; 36] = &mut [0; 36];
+    registers[0] = 0x00;
+
+    loop {
+        if (registers[0] + 16) as usize >= memory.len() {
+            println!("CPU fault: program counter (current vCPU location) is greater than the currently avail memory");
+            exit(1);
+        }
+
+        let instruction_fetch: &[u8] = cpu::fetch(memory, registers[0]);
+
+        let decoded_instruction = cpu::decode(instruction_fetch).unwrap();
+        registers[0] += decoded_instruction.argv_len as u32 + 1;
+
+        dbg!(registers[0]);
+        dbg!(decoded_instruction);
+    }
 }
