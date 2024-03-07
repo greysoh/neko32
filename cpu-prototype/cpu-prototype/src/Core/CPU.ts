0d4340a7355bbd666dea2677b714e6c5a513bc55
greysoh <me@greysoh.dev>
2024-03-07 09:58:55 -0500

    feature: Adds base CPU.
    Oops.

---
+++ cpu-prototype/cpu-prototype/src/Core/CPU.ts
@@ -0,0 +1,219 @@
+import type { Memory } from "./Memory";
+
+enum Opcodes {
+  NOP = 0,
+  RET,
+
+  EQL,
+  INV,
+  GHT,
+  REW,
+  MEW,
+  RMV,
+  MMV,
+
+  SPU,
+  SPE,
+  SPO,
+
+  LSB,
+  RSB,
+  NOT,
+  AND,
+  ORB,
+  XOR,
+
+  ADD,
+  SUB,
+  MUL,
+  DIV,
+  MOD
+}
+
+type Instruction = {
+  opcode: number,
+
+  arguments: number[],
+  argumentLen: number
+}
+
+function getInstructionLength(instruction: number): number {
+  if (instruction == Opcodes.NOP || instruction == Opcodes.SPO) return 0;
+
+  if (instruction == Opcodes.EQL || instruction == Opcodes.GHT) return 3;
+  if (instruction >= Opcodes.MEW && instruction <= Opcodes.MMV) return 3;
+
+  if (instruction == Opcodes.REW) return 5;
+
+  if (instruction == Opcodes.SPU || instruction == Opcodes.SPO) return 1;
+  if (instruction >= Opcodes.LSB && instruction <= Opcodes.MOD) return 3;
+
+  throw new Error("Out of range");
+}
+
+function u32ToInt(list: number[]): number {
+  return list[0] * (Math.pow(256, 3)) + list[1] * (Math.pow(256, 2)) + list[2] * 256 + list[3];
+}
+
+export class CPU {
+  memory: Memory;
+  registers: Uint32Array;
+
+  constructor(memory: Memory, registers: Uint32Array) {
+    this.memory = memory;
+    this.registers = registers;
+  }
+
+  fetch(): number[] {
+    return this.memory.getBulk(this.registers[0], this.registers[0] + 8);
+  }
+
+  decode(fetchedInstruction: number[]): Instruction {
+    // TODO: am lazy.
+    
+    if (fetchedInstruction[0] == Opcodes.REW) {
+      const instructionLength = getInstructionLength(fetchedInstruction[0]);
+
+      return {
+        opcode: fetchedInstruction[0],
+        arguments: [u32ToInt(fetchedInstruction.slice(1, instructionLength)), fetchedInstruction[instructionLength + 1]],
+
+        argumentLen: instructionLength
+      }
+    } else {
+      const instructionLength = getInstructionLength(fetchedInstruction[0]);
+
+      return {
+        opcode: fetchedInstruction[0],
+        arguments: fetchedInstruction.slice(1, instructionLength + 1),
+
+        argumentLen: instructionLength
+      }
+    }
+  }
+
+  execute(instruction: Instruction): void {
+    switch (instruction.opcode) {
+      default: {
+        throw new Error("Illegal instruction")
+      }
+
+      case Opcodes.NOP: {
+        break;
+      }
+
+      case Opcodes.EQL: {
+        this.registers[instruction.arguments[2]] = Number(this.registers[instruction.arguments[0]] == this.registers[instruction.arguments[1]]);
+        break;
+      }
+
+      case Opcodes.INV: {
+        this.registers[instruction.arguments[1]] = Number(!this.registers[instruction.arguments[0]]);
+        break;
+      }
+
+      case Opcodes.GHT: {
+        this.registers[instruction.arguments[2]] = Number(instruction.arguments[0] > instruction.arguments[1])
+        break;
+      }
+
+      case Opcodes.REW: {
+        this.registers[instruction.arguments[1]] = instruction.arguments[0];
+        break;
+      }
+
+      case Opcodes.RMV: {
+        this.registers[instruction.arguments[1]] = this.registers[0];
+        break;
+      }
+
+      case Opcodes.MEW: {
+        this.memory.set(instruction.arguments[0], instruction.arguments[1]);
+        break;
+      }
+
+      case Opcodes.SPU: {
+        this.memory.set(this.registers[1], instruction.arguments[0]);
+        this.registers[1]++;
+
+        break;
+      }
+
+      case Opcodes.SPE: {
+        this.registers[instruction.arguments[0]] = this.memory.get(this.registers[1]);
+        break;
+      }
+
+      case Opcodes.SPO: {
+        this.registers[1]--;
+        break;
+      }
+      
+      case Opcodes.LSB: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] << this.registers[instruction.arguments[1]];
+        break;
+      }
+
+      case Opcodes.RSB: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] >> this.registers[instruction.arguments[1]];
+        break;
+      }
+      
+      case Opcodes.NOT: {
+        this.registers[instruction.arguments[1]] = ~this.registers[instruction.arguments[0]];
+        break;
+      }
+
+      case Opcodes.AND: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] & this.registers[instruction.arguments[1]];
+        break;
+      }
+
+      case Opcodes.ORB: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] | this.registers[instruction.arguments[1]];
+        break;
+      }
+
+      case Opcodes.XOR: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] ^ this.registers[instruction.arguments[1]];
+        break;
+      }
+
+      case Opcodes.ADD: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] + this.registers[instruction.arguments[1]];
+        break;
+      }
+
+      case Opcodes.SUB: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] - this.registers[instruction.arguments[1]];
+        break;
+      }
+
+      case Opcodes.MUL: {
+        this.registers[instruction.arguments[2]] = Math.floor(this.registers[instruction.arguments[0]] * this.registers[instruction.arguments[1]]);
+        break;
+      }
+
+      case Opcodes.DIV: {
+        this.registers[instruction.arguments[2]] = Math.floor(this.registers[instruction.arguments[0]] / this.registers[instruction.arguments[1]]);
+        break;
+      }
+
+      case Opcodes.DIV: {
+        this.registers[instruction.arguments[2]] = this.registers[instruction.arguments[0]] % this.registers[instruction.arguments[1]];
+        break;
+      }
+    }
+
+    this.registers[3] = 0;
+    this.registers[4] = 1;
+  }
+  
+  reset(): void {
+    this.memory.clear();
+  }
+
+  tick() {
+    this.execute(this.decode(this.fetch()));
+  }
+}
---
+++ cpu-prototype/cpu-prototype/src/Core/Memory.ts
@@ -0,0 +1,68 @@
+export const MMIOCallbackRead = 0x00;
+export const MMIOCallbackWrite = 0x01;
+
+type MMIOCallbackEvent = (event: number, address: number, value?: number) => number | undefined;
+
+export class Memory {
+  rawMemory: Uint8Array;
+
+  mmioEvents: {
+    callback: MMIOCallbackEvent,
+
+    startPos: number,
+    endPos:   number
+  }[];
+
+  constructor(memoryBase: Uint8Array) {
+    this.rawMemory = memoryBase;
+  }
+
+  get(pos: number): number {
+    const mmioEventsSearch = this.mmioEvents.find((i) => i.startPos <= pos && i.endPos >= pos);
+    
+    if (mmioEventsSearch) {
+      const output = mmioEventsSearch.callback(MMIOCallbackRead, pos);
+      if (!output) return 0;
+      
+      return output;
+    }
+
+    return this.rawMemory[pos];
+  }
+
+  getBulk(startPos: number, endPos: number): number[] {
+    const values: number[] = [];
+
+    for (let currentPos = startPos; currentPos < endPos; currentPos++) {
+      values.push(this.get(currentPos));
+    }
+
+    return values;
+  }
+
+  set(pos: number, value: number): void {
+    const mmioEventsSearch = this.mmioEvents.find((i) => i.startPos <= pos && i.endPos >= pos);
+    
+    if (mmioEventsSearch) {
+      mmioEventsSearch.callback(MMIOCallbackWrite, pos, value);
+      return;
+    }
+
+    this.rawMemory[pos] = value;
+  }
+
+  clear(resetMMIO?: boolean): void {
+    if (resetMMIO) this.mmioEvents.splice(0, this.mmioEvents.length);
+    
+    // FIXME: Let's hope the GC cleans this up....
+    this.rawMemory = new Uint8Array(this.rawMemory.length);
+  }
+
+  configureMMIO(startPos: number, endPos: number, callback: MMIOCallbackEvent): void {
+    this.mmioEvents.push({
+      callback,
+      startPos,
+      endPos
+    });
+  }
+}
