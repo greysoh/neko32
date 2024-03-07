4a47360371c2ba6f53a79af40152762216a3dac3
greysoh <me@greysoh.dev>
2024-03-07 09:58:36 -0500

    feature: Gets base CPU written.

--- asm/neko32/cpu.asm
+++ asm/neko32/cpu.asm
@@ -10,6 +10,8 @@
     equals  {registerEq0: register} {registerEq1: register} {registerOutput: register} => 0x02 @registerEq0 @registerEq1 @registerOutput
     invert  {registerToInvert: register} {registerOutput: register} => 0x03 @registerToInvert @registerOutput
     grt_thn {registerEq0: register} {registerEq1: register} {registerOutput: register} => 0x04 @registerEq0 @registerEq1 @registerOutput
+    
+    ; Memory
     reg_wri {value: u32} {register: register} => 0x05 @value @register
     mem_wri {register: register} {memory: register} => 0x06 @register @memory
     reg_mov {registerInput: register} {registerOutput: register} => 0x07 @registerInput @registerOutput
@@ -23,14 +25,14 @@
     ; Bitwise operations
     bit_left  {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x0c @registerFirst @registerSecond @registerOutput
     bit_right {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x0d @registerFirst @registerSecond @registerOutput
-    bit_not   {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x0e @registerFirst @registerSecond @registerOutput
+    bit_not   {registerFirst: register} {registerOutput: register} => 0x0e @registerFirst @registerSecond @registerOutput
     bit_and   {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x0f @registerFirst @registerSecond @registerOutput
     bit_or    {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x10 @registerFirst @registerSecond @registerOutput
     bit_xor   {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x11 @registerFirst @registerSecond @registerOutput
 
     ; Math
     math_add {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x12 @registerFirst @registerSecond @registerOutput
-    math_dec {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x13 @registerFirst @registerSecond @registerOutput
+    math_sub {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x13 @registerFirst @registerSecond @registerOutput
     math_mul {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x14 @registerFirst @registerSecond @registerOutput
     math_div {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x15 @registerFirst @registerSecond @registerOutput
     math_mod {registerFirst: register} {registerSecond: register} {registerOutput: register} => 0x16 @registerFirst @registerSecond @registerOutput
