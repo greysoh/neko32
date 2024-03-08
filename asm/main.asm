60fe56c5412f252eab9b264b82d574e1c2537c82
greysoh <me@greysoh.dev>
2024-03-08 08:03:34 -0500

    feature: Optimize assembly.

--- asm/main.asm
+++ asm/main.asm
@@ -3,27 +3,23 @@
 ; Demo program.
 
 main:
-  reg_wri 2 r1
+  reg_wri 1 r1
   reg_wri 2 r2
 
   .loop:
-    funct .main
-    jump .loop
-
-  .main:
-    funct .main_br
+    funct .reset_values
 
     math_mul r1 r2 r3
     reg_mov r3 r1
 
-    return c1
+    jump .loop
 
   ; Reset to default values if overflow occurs
-  .main_br:
+  .reset_values:
     equals c0 r1 r4
     invert r4 r5
 
     return r5
 
-    reg_wri 2 r1
+    reg_wri 1 r1
     return c1
