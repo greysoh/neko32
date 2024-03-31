435af520765afb55dafd6d1d712faf6cb94d4fd1
zero <rosascript@gmail.com>
2024-03-31 14:48:27 -0400

    fix suggestions from pr #8

--- cpu-prototype/src/debug_runner.ts
+++ cpu-prototype/src/debug_runner.ts
@@ -43,162 +43,159 @@ console.log('Happy debugging!\n');
 
 process.stdout.write('$ ');
 
+type Command = {
+  name: string;
+  description: string;
+  usage: string;
+  run: Function;
+};
+const commands: Command[] = [
+  {
+    name: 'clear',
+    description: 'clear the screen.',
+    usage: 'clear',
+    run() {
+      console.clear();
+    },
+  },
+  {
+    name: 'load-rom',
+    description: 'load a rom file.',
+    usage: 'load-rom <rom>',
+    async run(args: string[]) {
+      if (!args[1]) {
+        console.error('File not specified!');
+        return;
+      }
+
+      try {
+        const romFile = await readFile(args[1]);
+
+        if (romFile.length > 4096) {
+          console.error('File is too big!');
+          return;
+        }
+
+        file = romFile;
+      } catch (e) {
+        console.error('Error reading file!');
+        console.log(e);
+        return;
+      }
+
+      console.log('Loaded ROM file.');
+    },
+  },
+  {
+    name: 'step',
+    description: 'runs the cpu stepCount cycles. by default, one.',
+    usage: 'step [stepCount]',
+    run(args: string[]) {
+      const stepCount = args[1] ? parseInt(args[1]) : 1;
+
+      for (var i = 0; i < stepCount; i++) {
+        cpu.tick();
+      }
+    },
+  },
+  {
+    name: 'dis',
+    description: 'print current instruction that is being excuted.',
+    usage: 'dis',
+    run() {
+      const fetchedInstruction = cpu.fetch();
+      const decodedInstruction = cpu.decode(fetchedInstruction);
+
+      const foundInstructionOpcodes: string[] = Object.keys(Opcodes);
+      console.log(
+        foundInstructionOpcodes[decodedInstruction.opcode + 25].toLowerCase() +
+          ' ' +
+          decodedInstruction.arguments.join(' ')
+      );
+    },
+  },
+  {
+    name: 'mem_read',
+    description: 'read the memory.',
+    usage: 'mem_read <position>',
+    run(args: string[]) {
+      console.log(memory.get(parseInt(args[1])));
+    },
+  },
+  {
+    name: 'reg_read',
+    description: 'read a register.',
+    usage: 'reg_read <register>',
+    run(args: string[]) {
+      console.log(registers[parseInt(args[1])]);
+    },
+  },
+  {
+    name: 'mem_write',
+    description: 'write to the memory.',
+    usage: 'mem_write <position> <value>',
+    run(args: string[]) {
+      memory.set(parseInt(args[1]), parseInt(args[2]));
+    },
+  },
+  {
+    name: 'reg_write',
+    description: 'write to a register.',
+    usage: 'reg_write <register> <value>',
+    run(args: string[]) {
+      registers[parseInt(args[1])] = parseInt(args[2]);
+    },
+  },
+  {
+    name: 'branch_list',
+    description: 'get the list of internal cpu branches.',
+    usage: 'branch_list',
+    run() {
+      console.log(cpu.branchLog.join(' '));
+    },
+  },
+  {
+    name: 'help',
+    description: 'prints this help message.',
+    usage: 'help [command]',
+    run(args: string[]) {
+      if (!args[1]) {
+        console.log('command list:');
+        commands.forEach((element) => {
+          console.log(`${element.name}: ${element.description}`);
+        });
+      } else {
+        let commandid = commands.findIndex(
+          (el) => el.name.toLowerCase() == args[1]
+        );
+        let command = commands[commandid];
+        console.log(`${command.name}: ${command.description}`);
+        console.log(`usage: ${command.usage}`);
+      }
+    },
+  },
+  {
+    name: 'exit',
+    description: 'exits the console.',
+    usage: 'exit',
+    run() {
+      process.exit(0);
+    },
+  },
+];
+
 for await (const line of readline) {
   const splitCommand = line.split(' ');
 
-  type Command = {
-    name: string;
-    description: string;
-    usage: string;
-    run: Function;
-  };
-
-  let commands: Command[] = [
-    {
-      name: 'clear',
-      description: 'clear the screen.',
-      usage: 'clear',
-      run: () => {
-        console.clear();
-      },
-    },
-    {
-      name: 'load-rom',
-      description: 'load a rom file.',
-      usage: 'load-rom <rom>',
-      run: async () => {
-        if (!splitCommand[1]) {
-          console.error('File not specified!');
-          return;
-        }
-
-        try {
-          const romFile = await readFile(splitCommand[1]);
-
-          if (romFile.length > 4096) {
-            console.error('File is too big!');
-            return;
-          }
-
-          file = romFile;
-        } catch (e) {
-          console.error('Error reading file!');
-          console.log(e);
-          return;
-        }
-
-        console.log('Loaded ROM file.');
-      },
-    },
-    {
-      name: 'step',
-      description: 'runs the cpu stepCount cycles. by default, one.',
-      usage: 'step [stepCount]',
-      run: () => {
-        const stepCount = splitCommand[1] ? parseInt(splitCommand[1]) : 1;
-
-        for (var i = 0; i < stepCount; i++) {
-          cpu.tick();
-        }
-      },
-    },
-    {
-      name: 'dis',
-      description: 'print current instruction that is being excuted.',
-      usage: 'dis',
-      run: () => {
-        const fetchedInstruction = cpu.fetch();
-        const decodedInstruction = cpu.decode(fetchedInstruction);
-
-        const foundInstructionOpcodes: string[] = Object.keys(Opcodes);
-        console.log(
-          foundInstructionOpcodes[
-            decodedInstruction.opcode + 25
-          ].toLowerCase() +
-            ' ' +
-            decodedInstruction.arguments.join(' ')
-        );
-      },
-    },
-    {
-      name: 'mem_read',
-      description: 'read the memory.',
-      usage: 'mem_read <position>',
-      run: () => {
-        console.log(memory.get(parseInt(splitCommand[1])));
-      },
-    },
-    {
-      name: 'reg_read',
-      description: 'read a register.',
-      usage: 'reg_read <register>',
-      run: () => {
-        console.log(registers[parseInt(splitCommand[1])]);
-      },
-    },
-    {
-      name: 'mem_write',
-      description: 'write to the memory.',
-      usage: 'mem_write <position> <data>',
-      run: () => {
-        memory.set(parseInt(splitCommand[1]), parseInt(splitCommand[2]));
-      },
-    },
-    {
-      name: 'reg_write',
-      description: 'write to a register.',
-      usage: 'reg_write <register> <data>',
-      run: () => {
-        registers[parseInt(splitCommand[1])] = parseInt(splitCommand[2]);
-      },
-    },
-    {
-      name: 'branch_list',
-      description: 'get the list of internal cpu branches.',
-      usage: 'branch_list',
-      run: () => {
-        console.log(cpu.branchLog.join(' '));
-      },
-    },
-    {
-      name: 'help',
-      description: 'prints this help message.',
-      usage: 'help',
-      run: () => {
-        if (!splitCommand[1]) {
-          console.log('command list:');
-          commands.forEach((element) => {
-            console.log(`${element.name}: ${element.description}`);
-          });
-        } else {
-          let commandid = commands.findIndex(
-            (el) => el.name.toLowerCase() == splitCommand[1]
-          );
-          let command = commands[commandid];
-          console.log(`${command.name}: ${command.description}`);
-          console.log(`usage: ${command.usage}`);
-        }
-      },
-    },
-    {
-      name: 'exit',
-      description: 'exits the console.',
-      usage: 'exit',
-      run: () => {
-        process.exit(0);
-      },
-    },
-  ];
-
-  let commandid = commands.findIndex(
-    (el) => el.name.toLowerCase() == splitCommand[0]
-  );
-  let command = commands[commandid];
+  let command =
+    commands[
+      commands.findIndex((el) => el.name.toLowerCase() == splitCommand[0])
+    ];
 
   if (!command) {
     console.log("Unknown command. Run 'help' to get a list of all commands.");
   } else {
-    command.run();
+    command.run(splitCommand);
   }
 
   process.stdout.write('$ ');
