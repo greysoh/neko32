550c3c0f665e3ef06c408100c29e059542db9c36
greysoh <me@greysoh.dev>
2024-05-27 11:30:42 -0400

    chore: Rewrites u8 and u32 conversion tests.

--- compiler/test_input/hits_u8tou32_and_u32tou8.js
+++ compiler/test_input/hits_u8tou32_and_u32tou8.js
@@ -1,9 +1,10 @@
-function main() {
-  CPU.registers[6] = 4;
-  CPU.registers[7] = 6;
-  CPU.registers[8] = 12;
+import {} from "std:u32ToU8";
+import {} from "std:u8ToU32";
 
-  CPU.registers[9] = CPU.registers[6] + CPU.registers[7] + CPU.registers[8];
+function main() {
+  CPU.registers[33] = 2048;
+  u32ToU8();
+  u8ToU32();
 
   CPU.jump(nopLoop);
 }
